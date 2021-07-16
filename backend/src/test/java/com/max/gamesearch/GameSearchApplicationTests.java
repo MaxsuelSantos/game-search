891220df0c419a2a14c40ebcf4066d9806360227
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-07-16 14:12:12 -0300

    Backend created

---
+++ backend/src/test/java/com/max/gamesearch/GameSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.gamesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
